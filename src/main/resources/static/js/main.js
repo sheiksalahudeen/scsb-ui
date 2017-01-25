80b4bf47d8f7e4fd0ca211f4901b70683aff56a9
Hari-Krishnan19 <krish19nov@gmail.com>
2017-01-25 21:22:21 +0530

    Recap-351 Resolved viewport height and changed Font color in User Screen.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
         $.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
     });
     
-    var docheight = $(document).height();
+    var docheight = $(window).height();
     var headerheight = $('header .container-fluid').height();
     var footerheight = $('footer .container-fluid').height();
     var containerheight = (docheight - (headerheight + footerheight));
     $('section .container-fluid').css("min-height",(containerheight)+"px");
-        
+
 });
