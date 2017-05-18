2e6deff2e02914187a8dc37d79c202a882775293
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2017-05-18 08:38:07 -0400

    RECAP-686 : Modified  to allow only GET and POST request methods

--- src/main/java/org/recap/Main.java
+++ src/main/java/org/recap/Main.java
@@ -1,15 +1,22 @@
 package org.recap;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.recap.security.SessionFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -52,4 +59,37 @@ public class Main {
 		filterRegistrationBean.setFilter(new SessionFilter());
 		return filterRegistrationBean;
 	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				if (container.getClass().isAssignableFrom(TomcatEmbeddedServletContainerFactory.class)) {
+					TomcatEmbeddedServletContainerFactory tomcatContainer = (TomcatEmbeddedServletContainerFactory) container;
+					tomcatContainer.addContextCustomizers(new ContextSecurityCustomizer());
+				}
+			}
+		};
+	}
+
+	private static class ContextSecurityCustomizer implements TomcatContextCustomizer {
+		@Override
+		public void customize(Context context) {
+			SecurityConstraint constraint = new SecurityConstraint();
+			SecurityCollection securityCollection = new SecurityCollection();
+			securityCollection.setName("restricted_methods");
+			securityCollection.addPattern("/*");
+			securityCollection.addMethod(HttpMethod.OPTIONS.toString());
+			securityCollection.addMethod(HttpMethod.HEAD.toString());
+			securityCollection.addMethod(HttpMethod.PUT.toString());
+			securityCollection.addMethod(HttpMethod.PATCH.toString());
+			securityCollection.addMethod(HttpMethod.DELETE.toString());
+			securityCollection.addMethod(HttpMethod.TRACE.toString());
+			constraint.addCollection(securityCollection);
+			constraint.setAuthConstraint(true);
+			context.addConstraint(constraint);
+		}
+	}
+
 }
